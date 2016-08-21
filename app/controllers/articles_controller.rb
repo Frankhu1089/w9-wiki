b33fbbbe29e13c3d5eff9b2037da4449e9dfed3c
Frankhu <f55665566@gmail.com>
2016-08-21 15:14:59 +0800

    basci user&create -- w/o edit&delete

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user! , except: [:index, :show]
+
+  def index
+    @articles = Article.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @article = current_user.articles.build
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.save(article_params)
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    #code
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
